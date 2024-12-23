fbe8d6663dfa97a19f3f6f91dbdb7f730700b5c6
shimizu <reborn.titech@gmail.com>
2024-12-23 22:30:42 +0900

    add validation

--- backend/schemas/users.py
+++ backend/schemas/users.py
@@ -5,17 +5,17 @@ from pydantic import Field
 
 
 class UserBase(BaseSchema):
-    user_name: str | None = None
-    age: int | None = None
+    user_name: str = Field(..., max_length=50, description="ユーザー名(50文字以下)")
+    age: int = Field(..., ge=18, le=100, description="年齢(18歳以上100歳以下)")
 
 
 class UserCreate(UserBase):
-    password: str
+    password: str = Field(..., min_length=8, description="パスワード(8文字以上)")
 
 
 # Properties to receive via API on update
 class UserUpdate(UserBase):
-    password: str | None = None
+    password: str = Field(..., min_length=8, description="パスワード(8文字以上)")
 
 
 class UserResponse(UserBase):
