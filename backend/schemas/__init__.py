79b9978b8ff9747b6ad01332e1af81343edac945
shimizu <reborn.titech@gmail.com>
2024-12-28 18:20:17 +0900

    add share api for gmail address

--- backend/schemas/__init__.py
+++ backend/schemas/__init__.py
@@ -2,3 +2,4 @@ from .core import BaseSchema, PagingMeta, PagingQueryIn, SortQueryIn
 from .request_info import RequestInfoResponse
 from .token import Token, TokenPayload
 from .users import UserCreate, UserResponse, UsersPagedResponse, UserUpdate
+from .share import EmailSend
